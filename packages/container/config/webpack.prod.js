3cb1fecf4cc8c4b0f787b40a6dd415ccedff6745
Jeff Goes <jeff.goes@gympass.com>
2022-09-01 23:22:50 -0300

    adding environemtn variable to the right file

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,8 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env;
-console.log("DOMAINNNNN", JSON.stringify(domain))
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
