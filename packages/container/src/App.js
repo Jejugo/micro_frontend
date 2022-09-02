06fb1ed6cb674b767160bfe31a75e65373cd1768
Jeff Goes <jeff.goes@gympass.com>
2022-09-01 22:34:03 -0300

    debug

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
 export default function App() {
+  // fetch on start
+  React.useEffect(() => {
+    console.log(process.env.PRODUCTION_DOMAIN)
+  })
   return (
     <>
       <h1>Hey there!!!!!</h1>
